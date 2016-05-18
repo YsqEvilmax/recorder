a7360a71b56c424da742cbba0530c5301482353d
Ahmad Khudeish <ahmadkhudeish@gmail.com>
2016-05-18 16:57:29 +1200

    Revert "Added old files back"
    This reverts commit 7eeabb595dfaee1228866c6f6f4181cedfa19a4e.

--- templates/main.php
+++ templates/main.php
@@ -3,16 +3,12 @@
 style('recorder', 'style');
 style('recorder', 'recorder');
 
-/*script('recorder', 'script');
+script('recorder', 'script');
 script('recorder', 'bootstrap.min');
 script('recorder', 'EncoderDemo');
 script('recorder', 'EncoderWorker');
 script('recorder', 'jquery.min');
 script('recorder', 'WavAudioEncoder.min');
-*/
-script('recorder', 'gumadapter');
-script('recorder', 'MediaStreamRecorder');
-script('recorder', 'script');
 
 
 ?>
